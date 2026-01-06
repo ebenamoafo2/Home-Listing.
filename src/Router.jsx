93a379a9eec09f2678983903aa0cd1861ec031de
ebenamoafo <ebenamoafo222@gmail.com>
2026-01-06 07:38:01 +0000

    fix: resolve route conflict causing homepage misrender

--- src/Router.jsx
+++ src/Router.jsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
     errorElement: <NotFoundPage />,
     children: [
       {
-        path: "/",
+        index: true,
         element: <HomePage />,
       },
       {
