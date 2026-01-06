101d4c37235a8d4074b3ae7b169dd1ab6322167f
ebenamoafo <ebenamoafo222@gmail.com>
2026-01-06 07:17:49 +0000

    style: make listing card responsive across breakpoints for ListingFilters

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import { Outlet } from "react-router-dom";
 
-import Devbar from "@/components/Devbar/Devbar";
 import Navbar from "@/components/Navbar";
 
 const App = () => {
   return (
     <>
-      <div className="fixed bottom-0 left-0 top-0">
-        <Devbar />
-      </div>
-      <div className="ml-[700px]">
+      <div>
         <Navbar />
         <Outlet />
       </div>
--- src/components/ListingFilters.jsx
+++ src/components/ListingFilters.jsx
@@ -13,21 +13,32 @@ const ListingFilters = ({ onChange }) => {
   };
 
   return (
-    <div className="flex flex-row items-center justify-center gap-2">
+    <div className="flex w-full flex-col gap-3 sm:flex-row sm:items-center sm:justify-center">
+      {/* Search */}
       <Input
-        className="w-[400px]"
+        className="w-full sm:w-[300px] md:w-[400px]"
         placeholder="Search destinations"
         value={search}
         onChange={e => setSearch(e.target.value)}
       />
-      <DateRangePicker
-        value={dates}
-        onChange={setDates}
-        minDate={new Date()}
-        placeholder="Add dates"
-      />
-      <Stepper label="guest" value={guests} onChange={setGuests} />
-      <Button onClick={handleSubmit}>
+
+      {/* Dates */}
+      <div className="w-full sm:w-auto">
+        <DateRangePicker
+          value={dates}
+          onChange={setDates}
+          minDate={new Date()}
+          placeholder="Add dates"
+        />
+      </div>
+
+      {/* Guests */}
+      <div className="w-full sm:w-auto">
+        <Stepper label="guest" value={guests} onChange={setGuests} />
+      </div>
+
+      {/* Submit */}
+      <Button onClick={handleSubmit} className="w-full sm:w-auto">
         <Search className="h-4 w-4" />
       </Button>
     </div>
